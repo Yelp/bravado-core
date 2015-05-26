5fc441ec72e6fac93224d08d6dbc6bef9a72105e
Semir Patel <spatel@yelp.com>
2015-05-26 11:00:52 -0700

    Use basePath whem matching a request to an operation; Bump to 1.0.0-rc1

--- tests/spec/Spec/get_op_for_request_test.py
+++ tests/spec/Spec/get_op_for_request_test.py
@@ -1,8 +1,32 @@
-def test_found(petstore_spec):
+from bravado_core.spec import Spec
+
+
+def test_found_with_no_basepath(petstore_dict):
+    del petstore_dict['basePath']
+    petstore_spec = Spec.from_dict(petstore_dict)
     op = petstore_spec.get_op_for_request('GET', '/pet/{petId}')
     assert op == petstore_spec.resources['pet'].operations['getPetById']
 
 
-def test_not_found(petstore_spec):
+def test_not_found_with_no_basepath(petstore_dict):
+    del petstore_dict['basePath']
+    petstore_spec = Spec.from_dict(petstore_dict)
     op = petstore_spec.get_op_for_request('GET', '/foo/{fooId}')
+    assert op is None
+
+
+def test_found_with_basepath(petstore_spec):
+    op = petstore_spec.get_op_for_request('GET', '/v2/pet/{petId}')
+    assert op == petstore_spec.resources['pet'].operations['getPetById']
+
+
+def test_found_with_basepath_containing_trailing_slash(petstore_dict):
+    petstore_dict['basePath'] = '/v2/'
+    petstore_spec = Spec.from_dict(petstore_dict)
+    op = petstore_spec.get_op_for_request('GET', '/v2/pet/{petId}')
+    assert op == petstore_spec.resources['pet'].operations['getPetById']
+
+
+def test_not_found_with_basepath(petstore_spec):
+    op = petstore_spec.get_op_for_request('GET', '/v2/foo/{fooId}')
     assert op is None
