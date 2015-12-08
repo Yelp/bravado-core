c2b29240d96e77e819e9a67f18b8d1d87a4e82c7
Marcel <jmsander@mail.upb.de>
2015-12-08 16:45:41 +0100

    guess type if not given

--- bravado_core/spec.py
+++ bravado_core/spec.py
@@ -335,6 +335,11 @@ def post_process_spec(swagger_spec, on_container_callbacks):
 
         # fragment is guaranteed not to be a ref from this point onwards
         if is_dict_like(fragment):
+            if 'type' not in fragment:
+                if 'properties' in fragment:
+                    fragment['type'] = 'object'
+                elif 'items' in fragment:
+                    fragment['type'] = 'array'
             for key, value in iteritems(fragment):
                 fire_callbacks(fragment, key, path + [key])
                 descend(fragment[key], path + [key], visited_refs)
