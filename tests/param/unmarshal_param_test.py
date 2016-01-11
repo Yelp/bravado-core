76f0b5b49d87e0d225a72db01453274ad3fe5c2f
Semir Patel <analogue@yahoo.com>
2016-01-11 08:05:14 -0800

    Fix typo

--- tests/param/unmarshal_param_test.py
+++ tests/param/unmarshal_param_test.py
@@ -81,7 +81,7 @@ def test_query_array(empty_swagger_spec, array_param_spec):
 def test_optional_query_array_with_no_default(empty_swagger_spec,
                                               array_param_spec):
     array_param_spec['required'] = False
-    # Set to something other than 'mutli' because 'multi' is a no-op in
+    # Set to something other than 'multi' because 'multi' is a no-op in
     # unmarshal_collection_format()
     array_param_spec['collectionFormat'] = 'csv'
     param = Param(empty_swagger_spec, Mock(spec=Operation), array_param_spec)
