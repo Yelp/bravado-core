f476cbf78a7b0fca02659a1039c5e4fe471d792d
William Ting <wting@yelp.com>
2016-06-20 17:56:32 -0700

    Raise an exception when the required type field is missing for an schema_object_spec.

--- tests/unmarshal/unmarshal_schema_object_test.py
+++ tests/unmarshal/unmarshal_schema_object_test.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import copy
+import pytest
+
+from bravado_core.exception import SwaggerMappingError
 from bravado_core.spec import Spec
 from bravado_core.unmarshal import unmarshal_schema_object
 
@@ -26,3 +30,18 @@ def test_use_models_false(petstore_dict):
         {'id': 200, 'name': 'short-hair'})
 
     assert isinstance(result, dict)
+
+
+def test_bad_object_spec(petstore_dict):
+    petstore_spec = Spec.from_dict(petstore_dict, config={'use_models': False})
+    category_spec = copy.deepcopy(
+        petstore_spec.spec_dict['definitions']['Category']
+    )
+    # Type is a required field for objects.
+    category_spec['properties']['id'].pop('type')
+
+    with pytest.raises(SwaggerMappingError):
+        unmarshal_schema_object(
+            petstore_spec,
+            category_spec,
+            {'id': 200, 'name': 'short-hair'})
