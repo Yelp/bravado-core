a49ed30c508182885a494b559d8c655b8ec89472
Andreas Hug <andreas.hug@mailbox.org>
2016-07-22 21:52:21 +0200

    Fix type_validator_test according to comment in #64
    The schema passed to type_validator is a prop_schema in these test cases.
    https://github.com/Yelp/bravado-core/pull/64#discussion-diff-49881019

--- tests/swagger20_validator/type_validator_test.py
+++ tests/swagger20_validator/type_validator_test.py
@@ -39,20 +39,20 @@ def test_validate_when_not_a_parameter_schema(m_draft4_type_validator,
 @patch('jsonschema._validators.type_draft4')
 def test_skip_when_nullable_property_schema_and_value_is_None(
         m_draft4_type_validator, minimal_swagger_spec):
-    param_schema = {'name': 'foo', 'x-nullable': True, 'type': 'string'}
-    type_validator(
+    prop_schema = {'x-nullable': True, 'type': 'string'}
+    list(type_validator(
         minimal_swagger_spec,
         validator=None,
-        types=param_schema['type'],
+        types=prop_schema['type'],
         instance=None,  # property value
-        schema=param_schema)
+        schema=prop_schema))
     assert m_draft4_type_validator.call_count == 0
 
 
 @patch('jsonschema._validators.type_draft4')
 def test_validate_when_not_nullable_property_schema_and_value_is_None(
         m_draft4_type_validator, minimal_swagger_spec):
-    param_schema = {'name': 'foo', 'x-nullable': False, 'type': 'string'}
-    args = (None, param_schema['type'], None, param_schema)
-    type_validator(minimal_swagger_spec, *args)
+    prop_schema = {'x-nullable': False, 'type': 'string'}
+    args = (None, prop_schema['type'], None, prop_schema)
+    list(type_validator(minimal_swagger_spec, *args))
     m_draft4_type_validator.assert_called_once_with(*args)
