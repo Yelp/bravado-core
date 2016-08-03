4c36afe9e7b67d069691767466fe0c3f0d54908e
Sven Steinheißer <svens@yelp.com>
2016-08-03 13:55:34 +0200

    Return methods return value instead of None

--- tests/exception/wrap_exception_test.py
+++ tests/exception/wrap_exception_test.py
@@ -11,3 +11,11 @@ def test_exception_gets_correctly_wrapped():
     with pytest.raises(IOError) as excinfo:
         raise_assertion_error()
     assert 'bla' == str(excinfo.value)
+
+
+def test_return_value_when_no_exception():
+    @wrap_exception(IOError)
+    def do_not_raise_exception():
+        return 'bla'
+
+    assert 'bla' == do_not_raise_exception()
