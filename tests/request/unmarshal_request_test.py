31e4ad27310d263a2495b0fae93ec3b673d73908
Samuele Maci <maci@yelp.com>
2016-08-23 15:49:43 +0200

    Security Objects handling tests

--- tests/request/unmarshal_request_test.py
+++ tests/request/unmarshal_request_test.py
@@ -15,6 +15,7 @@ def test_request_with_path_parameter(petstore_spec):
     op = petstore_spec.resources['pet'].operations['getPetById']
     request_data = unmarshal_request(request, op)
     assert request_data['petId'] == 1234
+    assert request_data['api_key'] == 'key1'
 
 
 def test_request_with_no_parameters(petstore_spec):
