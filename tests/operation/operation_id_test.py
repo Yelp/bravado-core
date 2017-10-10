e40e7bfe212f1a2383d94d4af03b3240a7f6a7bf
Samuele Maci <maci@yelp.com>
2017-10-11 00:33:38 +0200

    Use cached_property

--- tests/operation/operation_id_test.py
+++ tests/operation/operation_id_test.py
@@ -1,32 +1,22 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from bravado_core.operation import Operation
-from bravado_core.spec import Spec
+from bravado_core.operation import _sanitize_operation_id
 
 
 def test_returns_operation_id_from_operation_spec():
-    spec = Spec(spec_dict={})
-    operation_spec = {'operationId': 'getPetById'}
-    operation = Operation(spec, '/pet/{petId}', 'get', operation_spec)
-    assert 'getPetById' == operation.operation_id
+    assert 'getPetById' == _sanitize_operation_id('getPetById', 'get', '/pet/{petId}')
 
 
 def test_returns_generated_operation_id_when_missing_from_operation_spec():
-    spec = Spec(spec_dict={})
-    operation_spec = {}
-    operation = Operation(spec, '/pet', 'get', operation_spec)
-    assert 'get_pet' == operation.operation_id
+    assert 'get_pet' == _sanitize_operation_id(None, 'get', '/pet')
 
 
 def test_returns_generated_operation_id_with_path_parameters():
-    spec = Spec(spec_dict={})
-    operation_spec = {}
-    operation = Operation(spec, '/pet/{petId}', 'get', operation_spec)
-    assert 'get_pet_petId' == operation.operation_id
+    assert 'get_pet_petId' == _sanitize_operation_id(None, 'get', '/pet/{petId}')
 
 
-@pytest.mark.parametrize(('input', 'expected'), [
+@pytest.mark.parametrize(('operation_id', 'expected'), [
     ('pet.getBy Id', 'pet_getBy_Id'),      # simple case
     ('_getPetById_', 'getPetById'),        # leading/trailing underscore
     ('get__Pet_By__Id', 'get_Pet_By_Id'),  # double underscores
@@ -35,9 +25,5 @@ def test_returns_generated_operation_id_with_path_parameters():
     (' ', 'get_pet_petId'),                # crazy corner case 2
     ('_', 'get_pet_petId')                 # crazy corner case 3
 ])
-def test_returns_sanitized_operation_id_when_using_illegal_chars(
-        input, expected):
-    spec = Spec(spec_dict={})
-    operation_spec = {'operationId': input}
-    operation = Operation(spec, '/pet/{petId}', 'get', operation_spec)
-    assert expected == operation.operation_id
+def test_returns_sanitized_operation_id_when_using_illegal_chars(operation_id, expected):
+    assert _sanitize_operation_id(operation_id, 'get', '/pet/{petId}') == expected
