d5ae09437ae5ad547b3c342a8564322c9365db83
Samuele Maci <maci@yelp.com>
2017-10-16 10:24:22 +0200

    Define number_of_objects as fixture for easier pytest-benchmark comparison

--- tests/profiling/conftest.py
+++ tests/profiling/conftest.py
@@ -6,13 +6,17 @@ import pytest
 from bravado_core.spec import Spec
 
 
-NUMBER_OF_OBJECTS = [100, 1000, 5000]
+@pytest.fixture(
+    params=[100, 1000, 5000],
+)
+def number_of_objects(request):
+    return request.param
 
 
-@pytest.fixture(params=NUMBER_OF_OBJECTS, scope='module')
-def small_pets(request):
+@pytest.fixture
+def small_pets(number_of_objects):
     pets = []
-    for i in range(request.param):
+    for i in range(number_of_objects):
         pets.append({
             'name': str(uuid.uuid4()),
             'photoUrls': [str(uuid.uuid4())]
@@ -20,10 +24,10 @@ def small_pets(request):
     return pets
 
 
-@pytest.fixture(params=NUMBER_OF_OBJECTS, scope='module')
-def large_pets(request):
+@pytest.fixture
+def large_pets(number_of_objects):
     pets = []
-    for i in range(request.param):
+    for i in range(number_of_objects):
         pets.append({
             'id': i + 1,
             'name': str(uuid.uuid4()),
