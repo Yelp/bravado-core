063f105f0a6a5a62c838722814bf40d76a9da987
Samuele Maci <macisamuele@gmail.com>
2018-06-29 15:49:37 +0200

    Wrap _run_post_processing instead of mocking it out

--- tests/model/model_discovery_test.py
+++ tests/model/model_discovery_test.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 import mock
+import pytest
 
-from bravado_core.model import _collect_models
+from bravado_core.model import _run_post_processing
 from bravado_core.model import model_discovery
 from bravado_core.spec import Spec
 
 
-@mock.patch('bravado_core.model._run_post_processing', autospec=True)
-def test_model_discovery_flow_no_ref_dereference(mock__run_post_processing, minimal_swagger_dict):
+@pytest.fixture
+def wrap__run_post_processing():
+    with mock.patch(
+        'bravado_core.model._run_post_processing',
+        wraps=_run_post_processing,
+    ) as _wrap__run_post_processing:
+        yield _wrap__run_post_processing
+
+
+def test_model_discovery_flow_no_ref_dereference(wrap__run_post_processing, minimal_swagger_dict):
     spec = Spec(
         spec_dict=minimal_swagger_dict,
         config={
@@ -15,11 +24,10 @@ def test_model_discovery_flow_no_ref_dereference(mock__run_post_processing, mini
         },
     )
     model_discovery(swagger_spec=spec)
-    mock__run_post_processing.assert_called_once_with(spec)
+    wrap__run_post_processing.assert_called_once_with(spec)
 
 
-@mock.patch('bravado_core.model._run_post_processing', autospec=True)
-def test_model_discovery_flow_with_ref_dereference_with_no_definitions(mock__run_post_processing, minimal_swagger_dict):
+def test_model_discovery_flow_with_ref_dereference_with_no_definitions(wrap__run_post_processing, minimal_swagger_dict):
     spec = Spec(
         spec_dict=minimal_swagger_dict,
         config={
@@ -32,11 +40,10 @@ def test_model_discovery_flow_with_ref_dereference_with_no_definitions(mock__run
     # _run_post_processing is called 3 times
     # 1. post processing on initial specs
     # 2. post processing on on bravado_core.spec_flattening.flattened_spec
-    assert mock__run_post_processing.call_count == 2
+    assert wrap__run_post_processing.call_count == 2
 
 
-@mock.patch('bravado_core.model._run_post_processing', autospec=True)
-def test_model_discovery_flow_with_ref_dereference_with_definitions(mock__run_post_processing, minimal_swagger_dict):
+def test_model_discovery_flow_with_ref_dereference_with_definitions(wrap__run_post_processing, minimal_swagger_dict):
     spec = Spec(
         spec_dict=dict(minimal_swagger_dict, definitions={'model': {'type': 'object'}}),
         config={
@@ -44,25 +51,10 @@ def test_model_discovery_flow_with_ref_dereference_with_definitions(mock__run_po
         },
         origin_url='',
     )
-
-    def _run_post_processing_side_effect(swagger_spec):
-        _collect_models(
-            container={
-                'type': 'object',
-                'x-model': 'mock_model',
-            },
-            json_reference='#/definitions/mock_model/x-model',
-            models=spec.definitions,
-            swagger_spec=swagger_spec,
-        )
-
-    # Side effect is needed to ensure that at least a model has been discovered before
-    # flattening process
-    mock__run_post_processing.side_effect = _run_post_processing_side_effect
     model_discovery(swagger_spec=spec)
 
     # _run_post_processing is called 3 times
     # 1. post processing on initial specs
     # 2. post processing on on bravado_core.spec_flattening.flattened_spec
     # 3. post processing to rebuild definitions to remove possible references in the model specs
-    assert mock__run_post_processing.call_count == 3
+    assert wrap__run_post_processing.call_count == 3
