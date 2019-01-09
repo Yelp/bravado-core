023d64d3e1a391edb96dd16391abbef3d4dcc89f
Samuele Maci <macisamuele@gmail.com>
2019-01-09 20:42:39 +0100

    Add typing to bravado_core.security_definition

--- bravado_core/security_definition.py
+++ bravado_core/security_definition.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import logging
 
+import typing
+
+
+if typing.TYPE_CHECKING:
+    from bravado_core.spec import Spec  # noqa: F401
+
+    T = typing.TypeVar('T')
+
 log = logging.getLogger(__name__)
 
 
@@ -13,36 +21,49 @@ class SecurityDefinition(object):
     """
 
     def __init__(self, swagger_spec, security_definition_spec):
+        # type: (Spec, typing.Mapping[typing.Text, typing.Any]) -> None
         self.swagger_spec = swagger_spec
         self.security_definition_spec = swagger_spec.deref(security_definition_spec)
 
     @property
     def location(self):
+        # type: () -> typing.Optional[typing.Text]
         # not using 'in' as the name since it is a keyword in python
         return self.security_definition_spec.get('in')
 
     @property
     def type(self):
+        # type: () -> typing.Text
         return self.security_definition_spec['type']
 
     @property
     def name(self):
+        # type: () -> typing.Optional[typing.Text]
         return self.security_definition_spec.get('name')
 
     @property
     def flow(self):
+        # type: () -> typing.Optional[typing.Text]
         return self.security_definition_spec.get('flow')
 
     @property
     def scopes(self):
+        # type: () -> typing.Optional[typing.List[typing.Text]]
         return self.security_definition_spec.get('scopes')
 
     @property
     def authorizationUrl(self):
+        # type: () -> typing.Optional[typing.Text]
         return self.security_definition_spec.get('authorizationUrl')
 
     @property
+    def tokenUrl(self):
+        # type: () -> typing.Optional[typing.Text]
+        return self.security_definition_spec.get('tokenUrl')
+
+    @property
     def parameter_representation_dict(self):
+        # type: () -> typing.Optional[typing.Mapping[typing.Text, typing.Any]]
         if self.type == 'apiKey':
             return {
                 'required': False,
@@ -51,3 +72,5 @@ class SecurityDefinition(object):
                 'name': self.name,
                 'in': self.location,
             }
+        else:
+            return None
