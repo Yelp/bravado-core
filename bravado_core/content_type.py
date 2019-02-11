d62b304280b5df82c2e64e3d34d65b1a472913c9
Greg Ruane <greg.ruane@greenphire.com>
2019-02-11 11:06:42 -0500

    Add content types for excel, octet-stream

--- bravado_core/content_type.py
+++ bravado_core/content_type.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
+APP_EXCEL = 'application/vnd.ms-excel'
+APP_EXCEL_OPENXML = (
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+)
 APP_JSON = 'application/json'
 APP_MSGPACK = 'application/msgpack'
+APP_OCTET = 'application/octet-stream'
