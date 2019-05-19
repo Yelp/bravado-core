b6e2d2ca1275dc6a751fc866d89fd06c5c8b7eab
Samuele Maci <macisamuele@gmail.com>
2019-05-20 00:21:04 +0200

    Define helper method to detect type from schema object

--- bravado_core/_compat.py
+++ bravado_core/_compat.py
@@ -5,3 +5,9 @@ if six.PY2:  # pragma: no cover  # py2
     from functools32 import wraps  # noqa: F401
 else:  # pragma: no cover  # py3+
     from functools import wraps  # noqa: F401
+
+
+try:
+    from collections.abc import Mapping  # noqa: F401  # pragma: no cover  # py3.3+
+except ImportError:
+    from collections import Mapping  # noqa: F401  # py3.2 or older
