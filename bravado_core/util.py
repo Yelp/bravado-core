1b12f585fb165f860395fa6cf7ff0526c6365f9b
Samuele Maci <macisamuele@gmail.com>
2019-05-21 10:28:40 +0200

    Revert "Merge pull request #329 from macisamuele/maci-cached-property-enhancement"
    This reverts commit 89774d2af9c3dc81f40f9a73d7bad77207166ea5, reversing
    changes made to ed6c04020adb12edb6d099be23e786ef5cf676cb.

--- bravado_core/util.py
+++ bravado_core/util.py
@@ -48,9 +48,7 @@ class cached_property(object):
     def __get__(self, obj, cls):
         if obj is None:
             return self
-
-        value = self.func(obj)
-        setattr(obj, self.func.__name__, value)
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
         return value
 
 
