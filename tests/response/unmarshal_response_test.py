3357399eca54a37c74419cd8f064b7382a7f7ee5
Samuele Maci <macisamuele@gmail.com>
2019-05-22 16:49:10 +0200

    Ensure validation errors in case of response validation of an invalid discriminator (polymorphic schema)

--- tests/response/unmarshal_response_test.py
+++ tests/response/unmarshal_response_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import msgpack
 import pytest
+from jsonschema import ValidationError
 from mock import Mock
 from mock import patch
 
@@ -110,3 +111,55 @@ def test_performs_validation(empty_swagger_spec, response_spec):
             op = Mock(swagger_spec=empty_swagger_spec)
             unmarshal_response(response, op)
             assert val_schem.call_count == 1
+
+
+def test_unmarshal_model_polymorphic_specs(polymorphic_spec):
+    pet_list_dicts = [
+        {
+            'name': 'a dog name',
+            'type': 'Dog',
+            'birth_date': '2017-03-09',
+        },
+        {
+            'name': 'a cat name',
+            'type': 'Cat',
+            'color': 'white',
+        },
+    ]
+    pet_list_models = unmarshal_response(
+        response=Mock(
+            spec=IncomingResponse,
+            status_code=200,
+            headers={'content-type': APP_JSON},
+            json=Mock(return_value=pet_list_dicts),
+        ),
+        op=polymorphic_spec.resources['pets'].operations['get_pets'],
+    )
+
+    assert len(pet_list_dicts) == len(pet_list_models)
+
+    for list_item_model, list_item_dict in zip(pet_list_models, pet_list_dicts):
+        assert isinstance(list_item_model, polymorphic_spec.definitions['GenericPet'])
+        assert isinstance(list_item_model, polymorphic_spec.definitions[list_item_dict['type']])
+        assert list_item_model._marshal() == list_item_dict
+
+
+def test_unmarshal_model_polymorphic_specs_with_invalid_discriminator(polymorphic_spec):
+    pet_list_dicts = [
+        {
+            'name': 'a dog name',
+            'type': 'a-random-value',
+            'birth_date': '2017-03-09',
+        },
+    ]
+    with pytest.raises(ValidationError):
+        # Expecting validation error as "a-random-value" is not a valid type
+        unmarshal_response(
+            response=Mock(
+                spec=IncomingResponse,
+                status_code=200,
+                headers={'content-type': APP_JSON},
+                json=Mock(return_value=pet_list_dicts),
+            ),
+            op=polymorphic_spec.resources['pets'].operations['get_pets'],
+        )
