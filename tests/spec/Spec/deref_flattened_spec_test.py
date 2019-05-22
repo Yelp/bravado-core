4c3270c16ad69cdbbdde48bab315426ef8a0e762
Samuele Maci <macisamuele@gmail.com>
2019-05-22 17:38:26 +0200

    Remove un-needed xfail annotation

--- tests/spec/Spec/deref_flattened_spec_test.py
+++ tests/spec/Spec/deref_flattened_spec_test.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import pytest
 from six import iterkeys
 from six import itervalues
 
@@ -38,7 +37,6 @@ def _equivalent(spec, obj1, obj2):
         return obj1 == obj2
 
 
-@pytest.mark.xfail(reason='Flaky test, issue #219')
 def test_deref_flattened_spec_not_recursive_specs(petstore_spec):
     spec_dict = petstore_spec.spec_dict
     deref_spec_dict = petstore_spec.deref_flattened_spec
