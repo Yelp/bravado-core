2ea5323fde2700ce171db1bc5d60bb9b2e471777
Samuele Maci <macisamuele@gmail.com>
2019-12-07 08:53:34 +0100

    Explicitly xfail deecopy tests on Python 3.5.0

--- tests/resource/deepcopy_test.py
+++ tests/resource/deepcopy_test.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import sys
+
 import pytest
 
 from tests.conftest import check_object_deepcopy
@@ -9,5 +11,12 @@ def pet_resource(petstore_spec):
     return petstore_spec.resources['pet']
 
 
+@pytest.mark.xfail(
+    condition=sys.version_info[:3] == (3, 5, 0),
+    reason=(
+        "Deepcopy of lru_cache-ed items does not work properly "
+        "on Python 3.5.0. https://bugs.python.org/issue25447",
+    ),
+)
 def test_resource_instance_is_deep_copyable(pet_resource):
     check_object_deepcopy(pet_resource)
--- tests/spec/Spec/deepcopy_test.py
+++ tests/spec/Spec/deepcopy_test.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
+import sys
+
+import pytest
+
 from tests.conftest import check_object_deepcopy
 
 
+@pytest.mark.xfail(
+    condition=sys.version_info[:3] == (3, 5, 0),
+    reason=(
+        "Deepcopy of lru_cache-ed items does not work properly "
+        "on Python 3.5.0. https://bugs.python.org/issue25447",
+    ),
+)
 def test_spec_instance_is_deep_copyable(petstore_spec):
     check_object_deepcopy(petstore_spec)
