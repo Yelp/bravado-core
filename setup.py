531365ef1e5745cdf3d80391fd01eba04dffc4b3
Semir Patel <spatel@yelp.com>
2020-09-29 10:41:22 -0700

    Add python3.6 back in

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
     install_requires=[
