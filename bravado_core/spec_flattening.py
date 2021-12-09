3fb574ba45863e6dc100fd399d4f9049d056c373
Kwist <velnik@gmail.com>
2021-12-09 15:38:34 +0200

    Use some parameters from the internal spec in the `_SpecFlattener.model_discovery`

--- bravado_core/spec_flattening.py
+++ bravado_core/spec_flattening.py
@@ -318,6 +318,9 @@ class _SpecFlattener(object):
                         for uri, value in iteritems(self.known_mappings['definitions'])
                     },
                 },
+                origin_url=self.swagger_spec.origin_url,
+                http_client=self.swagger_spec.http_client,
+                config=self.swagger_spec.config,
             ),
         )
 
